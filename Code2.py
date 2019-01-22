113db60adadc53ed98c41e9ee7b40dacc314955e
1The1King1 <46817071+1The1King1@users.noreply.github.com>
2019-01-21 22:24:15 -0300

    Add files via upload

---
+++ Code2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+from time import sleep
+class Robot(object):
+    def __init__(self) :
+        self.MD = LargeMotor("outD")
+        self.ME = LargeMotor("outC")
+        self.IR = InfraredSensor()
+        self.GY = GyroSensor()
+
+        self.IR.mode = "IR-PROX"
+        self.GY.mode = "GYRO-ANG"
+
+    def Led(self) :
+        while True :
+            self.Leds.set_color(Leds.RIGHT, Leds.ORANGE)
+            sleep(1)
+    
+    def GYRO(self) :
+        print(self.GY.value())
+        Ang = sef.GY.value() + 87
+        while Ang != self.GY.value() :
+            self.MD.run_forever(speed_sp = 300)
+            self.ME.run_forever(speed_sp = 100)
+            self.Led()
+        
+    def Direita(self) :
+        if self.IR.value() <= 10 :
+            self.GYRO()
+    
+    def Main(self) :
+        self.MD.run_forever(speed_sp = 300)
+        self.ME.run_forever(speed_sp = 300)
+        self.Direita()
+Robot().Main()
